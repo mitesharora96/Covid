8d7af739dc0365b811bfc15d4726ee10d37646a8
nehamattani <nehamattani@gmail.com>
2020-07-25 12:26:20 +0530

    added rourting for blog component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {CovidDataService} from '../covid-data.service'
+import { CovidDataService } from '../covid-data.service'
 
-
-export interface RowData{
-  state:string;
-  confirmed:string;
-  active:string;
-  recovered:string;
-  deaths:string
+export interface RowData {
+  state: string;
+  confirmed: string;
+  active: string;
+  recovered: string;
+  deaths: string
 }
 
 @Component({
@@ -19,14 +18,15 @@ export interface RowData{
 
 export class HomeComponent implements OnInit {
 
-  DistrictData:any;
-  StateData:any;
-  rowData:RowData[]=[];
-  Searchvalue:string;
-  temp="Andaman and Nicobar Islands"
-  gridAPI:any;
-  detailCellRendererParams:any;
-  constructor(private db:CovidDataService) {
+  DistrictData: any;
+  StateData: any;
+  rowData: RowData[] = [];
+  Searchvalue: string;
+  temp = "Andaman and Nicobar Islands"
+  gridAPI: any;
+  detailCellRendererParams: any;
+
+  constructor(private db: CovidDataService) {
     this.detailCellRendererParams = {
       detailGridOptions: {
         columnDefs: [
@@ -36,46 +36,48 @@ export class HomeComponent implements OnInit {
           // { field: 'recovered'},
           // { field: 'deceased'},
         ],
-        
+
       },
-      getDetailRowData: function(params) {
+      getDetailRowData: function (params) {
         console.log(params.data.state);
         params.successCallback(this.DistrictData[params.data.state]);
       },
     };
-   }
+  }
 
   columnDefs = [
-    {headerName: 'State', field: 'state', sortable: true,cellRenderer: 'agGroupCellRenderer' },
-    {headerName: 'Confirmed', field: 'confirmed', sortable: true},
-    {headerName: 'Active', field: 'active', sortable: true},
-    {headerName: 'Recovered', field: 'recovered', sortable: true},
-    {headerName: 'Deceased', field: 'deaths', sortable: true}
-];
+    { headerName: 'State', field: 'state', sortable: true, cellRenderer: 'agGroupCellRenderer' },
+    { headerName: 'Confirmed', field: 'confirmed', sortable: true },
+    { headerName: 'Active', field: 'active', sortable: true },
+    { headerName: 'Recovered', field: 'recovered', sortable: true },
+    { headerName: 'Deceased', field: 'deaths', sortable: true }
+  ];
 
 
 
   ngOnInit(): void {
 
     this.db.getDistrictData().subscribe(
-      (data)=> {this.DistrictData=data;
-      console.log(this.DistrictData[this.temp].districtData)
+      (data) => {
+        this.DistrictData = data;
+        console.log(this.DistrictData[this.temp].districtData)
       }
     )
 
     this.db.getStatewiseData().subscribe(
-      data=>{this.StateData=data;      
-        }
+      data => {
+        this.StateData = data;
+      }
     )
   }
-  
-  
 
-  OnGridReady(parsar){
-    this.gridAPI=parsar.api;
+
+
+  OnGridReady(parsar) {
+    this.gridAPI = parsar.api;
   }
 
-  quickSearch(){
+  quickSearch() {
     this.gridAPI.setQuickFilter(this.Searchvalue);
   }
 }
